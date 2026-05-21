a881cabe3a5cc0a353bb619788e8b647164583b0
Dirk Scheffler <dirk.scheffler@modularmind.eu>
2026-05-21 08:52:38 +0200

    reflection improvements

--- rbac-api-model/src/tribefire/extension/auth/rbac/model/api/ServiceAuthorizationReflectionRequest.java
+++ rbac-api-model/src/tribefire/extension/auth/rbac/model/api/ServiceAuthorizationReflectionRequest.java
@@ -4,9 +4,10 @@ import com.braintribe.model.generic.annotation.Abstract;
 import com.braintribe.model.generic.reflection.EntityType;
 import com.braintribe.model.generic.reflection.EntityTypes;
 import com.braintribe.model.service.api.AuthorizedRequest;
+import com.braintribe.model.service.api.DomainRequest;
 
 @Abstract
-public interface ServiceAuthorizationReflectionRequest extends AuthorizedRequest {
+public interface ServiceAuthorizationReflectionRequest extends AuthorizedRequest, DomainRequest {
 	EntityType<ServiceAuthorizationReflectionRequest> T = EntityTypes.T(ServiceAuthorizationReflectionRequest.class);
 }
 
--- rbac-processing/src/tribefire/extension/auth/rbac/processing/ServiceDomainAuthorizationResolver.java
+++ rbac-processing/src/tribefire/extension/auth/rbac/processing/ServiceDomainAuthorizationResolver.java
@@ -1,32 +1,63 @@
 package tribefire.extension.auth.rbac.processing;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 import com.braintribe.model.generic.reflection.EntityType;
+import com.braintribe.model.generic.reflection.Model;
+import com.braintribe.model.meta.GmEntityType;
 import com.braintribe.model.processing.meta.cmd.CmdResolver;
 import com.braintribe.model.processing.meta.cmd.builders.EntityMdResolver;
+import com.braintribe.model.processing.meta.oracle.EntityTypeOracle;
 import com.braintribe.model.service.api.AuthorizedRequest;
+import com.braintribe.model.service.api.CompositeRequest;
+import com.braintribe.model.service.api.ExecuteAuthorized;
+import com.braintribe.model.service.api.ExecuteInDomain;
+import com.braintribe.model.service.api.ServiceRequest;
 
 public class ServiceDomainAuthorizationResolver extends ServiceRequestAuthorizationResolver {
 	private CmdResolver mdResolver;
+	private static Set<EntityType<? extends ServiceRequest>> publicServiceApiModelRequests = Set.of(
+			CompositeRequest.T,
+			ExecuteInDomain.T,
+			ExecuteAuthorized.T
+	);
 	
 	public ServiceDomainAuthorizationResolver(CmdResolver mdResolver, Set<String> overrideRoles) {
 		super(overrideRoles);
 		this.mdResolver = mdResolver;
 	}
 
-	public Set<EntityType<? extends AuthorizedRequest>> getRequestTypes() {
-		Set<EntityType<? extends AuthorizedRequest>> requestTypes = mdResolver.getModelOracle().findEntityTypeOracle(AuthorizedRequest.T) //
-				.getSubTypes().transitive().onlyInstantiable().asTypes();
+	public Set<EntityTypeOracle> getRequestTypes() {
+		Set<EntityTypeOracle> requestTypes = new HashSet<>(mdResolver.getModelOracle().findEntityTypeOracle(ServiceRequest.T) //
+				.getSubTypes().transitive().onlyInstantiable().asEntityTypeOracles());
+		
+		Model serviceApiModel = ServiceRequest.T.getModel();
+		
+		for (var it = requestTypes.iterator(); it.hasNext();) {
+			var typeOracle = it.next();
+			
+			if (typeOracle.asType().getModel() == serviceApiModel && !publicServiceApiModelRequests.contains(typeOracle.asType()) )
+				it.remove();
+		}
+		
 		return requestTypes;
 	}
 	
 	public Set<ServiceAuthorization> resolveAll() {
 		Set<ServiceAuthorization> authorizations = new HashSet<>();
 		
-		for (var requestType : getRequestTypes()) {
-			authorizations.add(resolve(requestType));
+		for (var requestTypeOracle : getRequestTypes()) {
+			EntityType<? extends ServiceRequest> requestType = requestTypeOracle.asType();
+			if (AuthorizedRequest.T.isAssignableFrom(requestType)) {
+				EntityType<? extends AuthorizedRequest> authType = (EntityType<? extends AuthorizedRequest>)requestType;
+				authorizations.add(resolve(authType));
+			}
+			else {
+				GmEntityType gmEntityType = requestTypeOracle.asGmEntityType();
+				authorizations.add(new ServiceAuthorization(gmEntityType, Collections.emptySet(), Collections.emptySet(), Collections.emptySet()));
+			}
 		}
 		
 		return authorizations;
